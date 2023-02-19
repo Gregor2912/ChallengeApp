6ab2e0d6b01089d65540b9735b43e53060c6c2e1
Grzegorz Plisko <grzybek2013@o2.pl>
2023-02-19 20:31:31 +0100

    ZadanieDzien4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Tomasz";
+char sex = 'm';
+int age = 15;
+if(sex == 'k'&& age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Tomasz" && age > 30)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == 'm' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
