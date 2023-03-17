d03727eed5fd32b927c527f95465c366d4e2aaa4
Grzegorz Plisko <grzybek2013@o2.pl>
2023-03-17 14:12:38 +0100

    ZadDzien9

---
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTest.cs
@@ -0,0 +1,24 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class EmployeeTest
+    {
+        [Test]
+        public void ResultShouldCheckOperationMinMaxAverage()
+        {
+            var klient = new Employee("Ewa", "Soroka");
+            var klient2 = new Employee("Adam", "Soroka");
+            klient.AddGrade(5);
+            klient.AddGrade(-4);
+            klient2.AddGrade(4);
+            klient2.AddGrade(10);
+
+
+            var klientM = klient.GetStatistics();
+            var klientW = klient2.GetStatistics();
+            Assert.AreNotEqual(klientM.Min, klientW.Max);
+            Assert.AreEqual(klientW.Min, 4);
+            Assert.AreEqual(klientW.Average, 7);
+        }
+
+    }
+}
